2163f290706065f6d5e95acfd33cc23adb35a986
倪金荣 <nijinrong1998@163.com>
2019-04-19 12:00:07 +0800

    新增list查询

---
+++ day02/list_type.py
@@ -0,0 +1,12 @@
+def list_sel(a):
+    print(a)
+    print(a[3])
+    print(a[3:4])
+    print(a[4:])
+    print(a[0:4])
+    print(a[-1:0])
+
+
+if __name__ == '__main__':
+    alist = ['你是猪','哈哈哈','我知道你是猪','你真的是猪','hhhhhhh']
+    list_sel(alist)
